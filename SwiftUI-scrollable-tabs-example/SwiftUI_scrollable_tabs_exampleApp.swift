ffa91665672f611b4b8505a03c9e8b6fe7f5c328
mdalireza021 <mdalireza021@gmail.com>
2023-10-08 13:16:46 +0600

    Initial Commit

---
+++ SwiftUI-scrollable-tabs-example/SwiftUI_scrollable_tabs_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_scrollable_tabs_exampleApp.swift
+//  SwiftUI-scrollable-tabs-example
+//
+//  Created by BS1096 on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_scrollable_tabs_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
